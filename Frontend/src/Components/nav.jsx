afa37af8af6ef0027711e5d30f024c731dde047f
Anant3008 <sastrulaanant@email.com>
2025-03-20 17:17:51 +0530

    minor change

---
+++ Frontend/src/Components/nav.jsx
@@ -0,0 +1,48 @@
+import {NavLink} from 'react-router-dom'
+
+const Nav=()=>{
+    return(
+    <div className='flex items-center justify-between space-x-6 w-full  py-2'>
+        <div className='flex justify-between px-15'>
+        <NavLink 
+            to='/'
+            className={({ isActive }) =>
+                `px-4 py-2 hover:text-yellow-600 transition duration-300 border-b-2 ${
+                  isActive ? "border-blue-400" : "border-transparent"
+                }`
+              }            
+        >Home</NavLink>
+        <NavLink 
+            to='/test'
+            className={({ isActive }) =>
+                `px-4 py-2 hover:text-yellow-600 transition duration-300 border-b-2 ${
+                  isActive ? "border-blue-400" : "border-transparent"
+                }`
+              }  
+        >Start Assesment</NavLink>
+        <NavLink 
+            to='/about'   
+            className={({ isActive }) =>
+                `px-4 py-2 hover:text-yellow-600 transition duration-300 border-b-2 ${
+                  isActive ? "border-blue-400" : "border-transparent"
+                }`
+              }  
+        >About</NavLink>
+        </div>
+
+        <div className='flex justify-between'>
+        <NavLink 
+            to='/login'
+            className='border border-gray-300 px-4 py-2 rounded-md mx-3'
+        >Login</NavLink>
+        
+        <NavLink 
+            to='/signup'
+            className='bg-blue-600 text-white px-4 py-2 rounded-md'
+        >Sign Up</NavLink>
+        </div>
+    </div>
+    )
+}
+
+export default Nav
