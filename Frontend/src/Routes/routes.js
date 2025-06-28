0c23ff8a1ad39e5763c09754f46fd822298cd456
anant <sastrulaanant@gmail.com>
2025-06-28 20:25:38 +0530

    wrong code

--- Frontend/src/Routes/routes.js
+++
@@ -1,7 +0,0 @@
-import Landing from "../Pages/landing";
-import AboutMe from "../Pages/aboutMe";
-import Test from "../Pages/test";
-import SignUp from "../Pages/signup";
-import Login from "../Pages/login";
-import Profile from "../Pages/Profile";
-export {Landing,AboutMe,Test,SignUp,Login, Profile}
